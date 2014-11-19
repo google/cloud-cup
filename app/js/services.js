e24d5c3c1b2c242d8a086d5c206e8a04d2d60351
Sarah Smith <sarahgsmith@google.com>
2014-11-19 11:33:02 +0100

    Set and increment the game number.

--- app/js/services.js
+++ app/js/services.js
@@ -13,7 +13,7 @@
       return fbutil.syncArray('joinedPlayers', {endAt: null});
     }])
 
-    .service('gameRunner', function($timeout, gameTypeForRoom) {
+    .service('gameRunner', function($timeout, gameTypeForRoom, gameNumberForRoom) {
       this.GAMES = ["tap", "shake"];
       this.MAX_GAMES = 5;
       this.GAME_LENGTH = 5000; // 10 seconds
@@ -21,23 +21,30 @@
       this.startNewGame = function(roomId) {
         this.gameIndex = 0;
         this.gameType = gameTypeForRoom(roomId);
+        this.gameNumber = gameNumberForRoom(roomId);
+        this.setGameNumber(0);
         this.switchGame();
       };
 
       this.switchGame = function() {
-        if (this.gameIndex == this.MAX_GAMES) {
+        if (this.gameNumber.$value == this.MAX_GAMES) {
           // TODO Show game over screen with final scores
           console.log('game over');
           return;
         }
         // TODO update the scores when we switch the game
-        this.gameIndex++;
+        this.setGameNumber((this.gameNumber.$value || 0) + 1);
         var newGame = Math.floor((Math.random() * this.GAMES.length));
         this.gameType.$value = this.GAMES[newGame];
         this.gameType.$save();
 
-        console.log('game ' + this.gameIndex + ' is ' + this.GAMES[newGame]);
+        console.log('game ' + this.gameNumber.$value + ' is ' + this.GAMES[newGame]);
         $timeout(this.switchGame.bind(this), this.GAME_LENGTH);
+      };
+
+      this.setGameNumber = function(gameNumber) {
+        this.gameNumber.$value = gameNumber;
+        this.gameNumber.$save();
       };
     })
 
@@ -56,6 +63,12 @@
     .factory('gameTypeForRoom', ['fbutil', function(fbutil) {
       return function(roomId) {
         return fbutil.syncObject('room/' + roomId + '/game/type' );
+      };
+    }])
+
+    .factory('gameNumberForRoom', ['fbutil', function(fbutil) {
+      return function(roomId) {
+        return fbutil.syncObject('room/' + roomId + '/game/number' );
       };
     }]);
 
